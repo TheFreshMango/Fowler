3270d74c579221010728b8ddb2cdb6c4a28d3df8
FreshMango <fabian.dogendorf@t-online.de>
2018-05-22 16:34:05 +0200

    refactoring: PriceCode computes the price!

--- Fowler Refactor/src/de/dhbw/fowler/PriceCode.java
+++ Fowler Refactor/src/de/dhbw/fowler/PriceCode.java
@@ -4,28 +4,42 @@ public 	enum PriceCode {
 	
 	CHILDRENS(){
 		@Override
-		public double getAmount() {
-			return super.getAmount();
+		public double getAmount(int daysRented) {
+			
+			double amount = 1.5;
+			if (daysRented > 3) {
+				amount += (daysRented - 3) * 1.5;
+			}
+			
+			return amount;
 		}
 	},
 	
 	REGULAR() {
 		@Override
-		public double getAmount() {
-			return super.getAmount();
+		public double getAmount(int daysRented) {
+			
+			double amount = 2;
+			if (daysRented > 2) {
+				amount += (daysRented - 2) * 1.5;
+			}
+			
+			return amount;
 		}
 	},
 	
 	NEW_RELEASE() {
 		@Override
-		public double getAmount() {
-			return super.getAmount();
+		public double getAmount(int daysRented) {
+			double amount = 3;
+			
+			return amount;
 		}
 	};
 	
 	PriceCode() {}
 	
-	public double getAmount() {
+	public double getAmount(int daysRented) {
 		return 0;
 	}
 }
