81616813c9132fc1c5aad696d5da031a474b59db
FreshMango <fabian.dogendorf@t-online.de>
2018-05-22 16:41:03 +0200

    refactoring: Customer uses PriceCode to determine amount

--- Fowler Refactor/src/de/dhbw/fowler/Customer.java
+++ Fowler Refactor/src/de/dhbw/fowler/Customer.java
@@ -35,7 +35,7 @@ public class Customer {
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+			if ((each.getMovie().getPriceCode().equals(PriceCode.NEW_RELEASE)) && each.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
@@ -49,23 +49,9 @@ public class Customer {
 	}
 
 	private double amountFor(Rental each) {
-		double thisAmount = 0;
-		switch (each.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return each.getMovie()
+				.getPriceCode()
+				.getAmount(each.getDaysRented());
 	}
 
 }
